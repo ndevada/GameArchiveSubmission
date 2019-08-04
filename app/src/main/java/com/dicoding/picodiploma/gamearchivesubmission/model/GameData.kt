515039fdf79a1d4ea44c5185c4dc2a3f668264ab
ndevada <meld.mee@gmail.com>
2019-08-04 22:03:54 +0800

    menambahkan list baru

--- app/src/main/java/com/dicoding/picodiploma/gamearchivesubmission/model/GameData.kt
+++ app/src/main/java/com/dicoding/picodiploma/gamearchivesubmission/model/GameData.kt
@@ -53,7 +53,85 @@ object GameData {
             "Single player",
             "https://images.igdb.com/igdb/image/upload/t_cover_big/co1hl9.jpg",
             "Set sail on an all-new ONE PIECE adventure in ONE PIECE World Seeker! Luffy and the Straw Hat Pirates are back in a new story that puts players’ into Luffy’s point of view, as he battles and explores an expansive and seamless world. Open landscapes provide many paths for players to experience Luffy’s gum-gum abilities and swing into action."
-        )
+        ),
+        arrayOf(
+            "The Witcher 3: Wild Hunt",
+            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8fbbad7-1f4a-4b4a-bae7-9304e592ba8d/d809e44-f0eb809d-938b-457b-96c7-4b29377afbd2.jpg/v1/fill/w_900,h_507,q_75,strp/the_witcher_3_wild_hunt_wallpapers__45__by_sultan_alqasmi_d809e44-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTA3IiwicGF0aCI6IlwvZlwvYjhmYmJhZDctMWY0YS00YjRhLWJhZTctOTMwNGU1OTJiYThkXC9kODA5ZTQ0LWYwZWI4MDlkLTkzOGItNDU3Yi05NmM3LTRiMjkzNzdhZmJkMi5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.s7L56cP3jPeX8z7x2B9qJFmlQdrgMmVPE6yko8rk6rQ",
+            "Adventure, Role-playing (RPG)",
+            "Xbox One, PC (Microsoft Windows), PlayStation 4",
+            "May 19, 2015",
+            "CD Projekt RED",
+            "WB Games, Bandai Namco Entertainment, cdp.pl, 1C Compan, Spike ChunSoft",
+            "Ведьмак 3: Дикая охота",
+            "Single player",
+            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8fbbad7-1f4a-4b4a-bae7-9304e592ba8d/d809e44-f0eb809d-938b-457b-96c7-4b29377afbd2.jpg/v1/fill/w_900,h_507,q_75,strp/the_witcher_3_wild_hunt_wallpapers__45__by_sultan_alqasmi_d809e44-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NTA3IiwicGF0aCI6IlwvZlwvYjhmYmJhZDctMWY0YS00YjRhLWJhZTctOTMwNGU1OTJiYThkXC9kODA5ZTQ0LWYwZWI4MDlkLTkzOGItNDU3Yi05NmM3LTRiMjkzNzdhZmJkMi5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.s7L56cP3jPeX8z7x2B9qJFmlQdrgMmVPE6yko8rk6rQ",
+            "The Witcher: Wild Hunt is a story-driven, next-generation open world role-playing game set in a visually stunning fantasy universe full of meaningful choices and impactful consequences. In The Witcher you play as the professional monster hunter, Geralt of Rivia, tasked with finding a child of prophecy in a vast open world rich with merchant cities, viking pirate islands, dangerous mountain passes, and forgotten caverns to explore."
+        ),
+        arrayOf(
+            "God of War",
+            "https://cdn.vox-cdn.com/thumbor/wrdbHdaNbUZ92WvQVBjPcaVxj-E=/0x0:1357x1037/1200x800/filters:focal(571x411:787x627)/cdn.vox-cdn.com/uploads/chorus_image/image/59469955/687474703a2f2f692e696d6775722e636f6d2f476c537665734d2e6a7067.0.jpg",
+            "Adventure, Role-playing (RPG)",
+            "PlayStation 4",
+            "Apr 20, 2018",
+            "SIE Santa Monica Studio",
+            "Sony Interactive Entertainment",
+            "God of War 4 (Chronological order)",
+            "Single player",
+            "https://cdn.vox-cdn.com/thumbor/wrdbHdaNbUZ92WvQVBjPcaVxj-E=/0x0:1357x1037/1200x800/filters:focal(571x411:787x627)/cdn.vox-cdn.com/uploads/chorus_image/image/59469955/687474703a2f2f692e696d6775722e636f6d2f476c537665734d2e6a7067.0.jpg",
+            "It is a new beginning for Kratos. Living as a man, outside the shadow of the gods, he seeks solitude in the unfamiliar lands of Norse mythology. With new purpose and his son at his side, Kratos must fight for survival as powerful forces threaten to disrupt the new life he has created..."
+        ),
+        arrayOf(
+            "Persona 5",
+            "https://www.mobygames.com/images/covers/l/390017-persona-5-playstation-3-front-cover.png",
+            "Adventure, Platform, Role-playing (RPG)",
+            "PlayStation 3, PlayStation 4",
+            "Sep 15, 2016",
+            "Atlus, P Studio",
+            "Atlus, Atlus USA, Deep Silver",
+            "ペルソナ5",
+            "Single Player",
+            "https://www.mobygames.com/images/covers/l/390017-persona-5-playstation-3-front-cover.png",
+            "Persona 5, a turn-based JRPG with visual novel elements, follows a high school student with a criminal record for a crime he didn't commit. Soon he meets several characters who share similar fates to him, and discovers a metaphysical realm which allows him and his friends to channel their pent-up frustrations into becoming a group of vigilantes reveling in aesthetics and rebellion while fighting corruption."
+        ),
+        arrayOf(
+            "Red Dead Redemption 2",
+            "https://cdn.mos.cms.futurecdn.net/Rw3xpgvxyD4PyiSZZ3xeMP.jpg",
+            "Adventure, Role-playing (RPG), Shooter",
+            "Xbox One, PlayStation 4",
+            "Oct 26, 2018",
+            "Rockstar Studios",
+            "Rockstar Games, Take-Two Interactive",
+            "Red Dead Redemption II",
+            "Co-operative, Multiplayer, Single player",
+            "https://cdn.mos.cms.futurecdn.net/Rw3xpgvxyD4PyiSZZ3xeMP.jpg",
+            "Developed by the creators of Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale of life in America’s unforgiving heartland. The game's vast and atmospheric world will also provide the foundation for a brand new online multiplayer experience."
+        ),
+        arrayOf(
+            "The Last of Us",
+            "https://d26bwjyd9l0e3m.cloudfront.net/wp-content/uploads/2014/08/The-Last-of-Us-Featured-1.jpg",
+            "Adventure, Shooter",
+            "PlayStation 3",
+            "Jun 14, 2013",
+            "Naughty Dog",
+            "Sony Computer Entertainment, Inc.",
+            "TLoU (Acronym)",
+            "Multiplayer, Single player",
+            "https://d26bwjyd9l0e3m.cloudfront.net/wp-content/uploads/2014/08/The-Last-of-Us-Featured-1.jpg",
+            "A third person shooter/stealth/survival hybrid, in which twenty years after the outbreak of a parasitic fungus which takes over the neural functions of humans, Joel, a Texan with a tragic familial past, finds himself responsible with smuggling a fourteen year old girl named Ellie to a militia group called the Fireflies, while avoiding strict and deadly authorities, infected fungal hosts and other violent survivors."
+        ),
+        arrayOf(
+            "Paper Mario: The Thousand-Year Door",
+            "https://i0.wp.com/www.nintendo-insider.com/wp-content/uploads/2019/08/paper_mario_the_thousand_year_door_key_art.jpg?fit=1280%2C720&ssl=1",
+            "Adventure, Role-playing (RPG)",
+            "Nintendo GameCube",
+            "Jul 22, 2004",
+            "Intelligent Systems Co., Ltd.",
+            "Nintendo",
+            "(ペーパーマリオRPG",
+            "Single player",
+            "https://i0.wp.com/www.nintendo-insider.com/wp-content/uploads/2019/08/paper_mario_the_thousand_year_door_key_art.jpg?fit=1280%2C720&ssl=1",
+            "Time passes, the pages turn…and a new chapter unfolds in an unfamiliar land! Get ready for a two-dimensional role-playing adventure for the ages as Mario returns to paper form to discover a mystery that sleeps behind an ancient, legendary portal called the Thousand-Year Door. The quest is long, the dangers many, and this time, Mario will have to make full use of his papery qualities just to survive."
+            )
     )
 
     val listData: ArrayList<Game>
